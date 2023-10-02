a4d1e2e0a350cea99dd03a0917c7861c2cb9917b
Abhinav Kumar <kabhinav713@gmail.com>
2023-10-03 00:24:02 +0530

    [Abhinav]-adding Inheritance part

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using _3A_Practice;
+
+Car myCar = new Car();
+
+// Call the honk() method (From the Vehicle class) on the myCar object
+myCar.honk();
+// Display the value of the brand field (from the Vehicle class) and the value of the modelName from the Car class
+Console.WriteLine(myCar.brand + " " + myCar.modelName);
