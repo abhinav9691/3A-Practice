68cc5a15bbf521df3a55ed840261ce71b58c8e34
Abhinav Kumar <kabhinav713@gmail.com>
2023-10-06 08:54:32 +0530

    [Abhinav]-adding Encapsulation  part

--- Program.cs
+++ Program.cs
@@ -8,10 +8,27 @@
 //Console.WriteLine(myCar.brand + " " + myCar.modelName);
 
 
-Animal myAnimal = new Animal();  // Create a Animal object
-Animal myPig = new Pig();  // Create a Pig object
-Animal myDog = new Dog();  // Create a Dog object
+//Animal myAnimal = new Animal();  // Create a Animal object
+//Animal myPig = new Pig();  // Create a Pig object
+//Animal myDog = new Dog();  // Create a Dog object
 
-myAnimal.animalSound();
-myPig.animalSound();
-myDog.animalSound();
+//myAnimal.animalSound();
+//myPig.animalSound();
+//myDog.animalSound();
+
+
+DemoEncap obj = new DemoEncap();
+
+// calls set accessor of the property Name,
+// and pass "Ankita" as value of the
+// standard field 'value'
+obj.Name = "Abhinav";
+
+// calls set accessor of the property Age,
+// and pass "21" as value of the
+// standard field 'value'
+obj.Age = 21;
+
+// Displaying values of the variables
+Console.WriteLine(" Name : " + obj.Name);
+Console.WriteLine(" Age : " + obj.Age);
